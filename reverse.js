c68eb429c462436773be7a9109956d330b52ca72
SanaNasar <sanasehereen.cse@gmail.com>
2014-07-08 22:37:04 -0700

    Adding my basics js exercise

---
+++ reverse.js
@@ -0,0 +1,10 @@
+// Reverse A String 
+
+// //Write a program that will take a hard coded string and reverse it.
+
+
+function reverse(s) {   // Declaring a function
+  var inputString = "building"; 
+  for (var i = s.length - 1, j = 0; i >= 0; i--, j++)
+  return reverse(inputString); // Using 'for' loop 
+}
