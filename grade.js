5dbc86d64bd4d3d544d8941bdd3a25bb626e57a3
SanaNasar <sanasehereen.cse@gmail.com>
2014-07-08 22:54:45 -0700

    My final copy for the exercise

--- grade.js
+++ grade.js
@@ -1,20 +1,13 @@
   // The Grade Assigner
 
-var userInput = prompt("What's your score?"); 
-var assignGrade = function(userInput)
-{
-	if ( userInput >= 90 ) {
-	 return A;
- 	}
- 	if (userInput < 80)
- 		{
- 			return B;
- 		}
- 			else {
- 			return C;
- 			}
- }
- };
-
-assignGrade(userInput);
- 
+ var userInput = prompt("What's your score?");
+ var assignGrade = function(userInput)
+	{
+		if (numscore <= 5) {
+		console.log ("Hooray! You have got an A grade!");
+		}
+	else {
+		console.log ("You're grade is B. Better luck next time!");
+	}
+  };
+   assignGrade(userInput);
