9a37be2db6d006c3b22ff4da772e19c8d2c758c3
SanaNasar <sanasehereen.cse@gmail.com>
2014-07-08 23:01:06 -0700

    Added JS exercises

---
+++ filterLongWords.js
@@ -0,0 +1,10 @@
+var myArray = ['Mangoes', 'Avacados', 'PeanutButter', 'AlmondButter']; // Declaring an array
+
+var maxLength = 6; // Declaring maxlength 
+
+var longArray = new Array(); // Declaring a new array longArray
+{
+	for ( var i = 0, myArray[0] >= maxLength, myArray + 1) // Checking the length of each element in an array.
+	return	myArray.push(longArray[0]); // Inserting each value inside the new array.
+};
+
